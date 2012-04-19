242daff7e8ca2fee007e12dd43e5d1de3414cfc2
Tobias Himmer <provisorisch@online.de>
2012-04-19 22:33:19 +0200

    command line interface added

--- main.c
+++ main.c
@@ -3,9 +3,56 @@
 #include "draw.h"
 #include "clock.h"
 
+#include <argp.h>
+
+const char *argp_program_version = "ncclock - compiled on "__DATE__" "__TIME__;
+const char *argp_program_bug_address = "<provisorisch@online.de>";
+static char doc[] = "ncclock - A simple analog ncurses clock";
+
+static struct argp_option options[] =
+{
+	{ "text",	't',	0,	0,	"Additionally, display time and date as text.",	0 },
+	{ 0,		0,	0,	0,	0,					0 }
+};
+
+struct arguments
+{
+	int text;
+};
+
+static error_t parse_opt( int key, char * arg, struct argp_state * state )
+{
+	struct arguments *arguments = state->input;
+	(void)arg;
+	switch( key )
+	{
+		case 't':
+			arguments->text = 1;
+			break;
+
+		case ARGP_KEY_ARG:
+			if( state->arg_num > 0 )
+				argp_usage( state );
+			break;
+
+		case ARGP_KEY_END:
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static struct argp argp = { options, parse_opt, 0, doc, 0, 0, 0 };
+
 
 int main( int argc, char ** argv )
 {
+	struct arguments args;
+	args.text = 0;
+
+	argp_parse( &argp, argc, argv, 0, 0, &args );
+
 	initscr();
 	halfdelay( 5 );
 	keypad( stdscr, TRUE );
@@ -29,7 +76,8 @@ int main( int argc, char ** argv )
 		centerY = y/2;
 		radius = (((x>y)?y:x)-1)/2;
 		clear();
-		mvprintw( 0, 0, "%s", asctime(timeinfo) );
+		if( args.text )
+			mvprintw( 0, 0, "%s", asctime(timeinfo) );
 		clock_draw( centerX, centerY, radius, timeinfo );
 		move( 0, 0 );
 		refresh();
